c2303b91be2ff161ea2208522104be6c483d42f7
jhanca <jhancarlohigueracarvajal@gmail.com>
2023-10-31 08:03:34 -0500

    creacion clases persona y Encargado la clase persona es una clase padre, la clases Encargado hereda de ella.

---
+++ src/main/java/com/sena/solution/models/Persona.java
@@ -0,0 +1,82 @@
+package com.sena.solution.models;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+
+public class Persona {
+	
+	@NotBlank(message = "El nombre es obligatorio")
+	private String nombre;
+	
+	@NotBlank(message = "El apellido es obligatorio")
+	private String apellido;
+	
+	@NotBlank(message = "La cedula es obligatorio")
+	private String cedula;
+	
+	@NotBlank(message = "El telefono es obligatorio")
+	private String telefono;
+	
+	@Email(message = "El correo no es correcto", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
+	@NotEmpty(message = "El correo no puede estar vacio")
+	private String correo;
+	
+	public Persona() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Persona(@NotBlank(message = "El nombre es obligatorio") String nombre,
+			@NotBlank(message = "El apellido es obligatorio") String apellido,
+			@NotBlank(message = "La cedula es obligatorio") String cedula,
+			@NotBlank(message = "El telefono es obligatorio") String telefono,
+			@Email(message = "El correo no es correcto", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$") @NotEmpty(message = "El correo no puede estar vacio") String correo) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.cedula = cedula;
+		this.telefono = telefono;
+		this.correo = correo;
+	}
+
+	public String getName() {
+		return nombre;
+	}
+
+	public void setName(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	
+}

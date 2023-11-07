bb445998bdd0e5aacf295d46f7dd122b60732087
jhanca <jhancarlohigueracarvajal@gmail.com>
2023-11-07 08:38:10 -0500

    poner en el nav los enlaces

--- src/main/java/com/sena/solution/GestorDocumentalApplication.java
+++ src/main/java/com/sena/solution/GestorDocumentalApplication.java
@@ -1,13 +1,21 @@
 package com.sena.solution;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.sena.solution.models.Curia;
+import com.sena.solution.services.CuriaService;
+
 @SpringBootApplication
 public class GestorDocumentalApplication {
+	
 
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GestorDocumentalApplication.class, args);
+		
 	}
 
 }

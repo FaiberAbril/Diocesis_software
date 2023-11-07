7296641d4099223ecc3eb1fb6c9f347644abba73
jhanca <jhancarlohigueracarvajal@gmail.com>
2023-11-07 07:51:09 -0500

    implementacionde layout listarCuria, listarVicaria y sus respectivos formularios

--- src/main/java/com/sena/solution/controllers/views/ParroquiaView.java
+++ src/main/java/com/sena/solution/controllers/views/ParroquiaView.java
@@ -3,7 +3,7 @@ package com.sena.solution.controllers.views;
 public class ParroquiaView {
 
 	public static final String HOME = "index";
-	public static final String LISTP = "listaParroquias";
+	public static final String LISTP = "listarParroquias";
 	public static final String FORMP = "formularioCrearParroquia";
 	public static final String FORMUPP = "formularioActualizarParroquia";
 }
